076d5d71dfa886b7fa01163774a8b9f3d0d606ec
Shimmerpeace <peacedamian1@gmail.com>
2025-03-26 13:17:31 +0100

    update

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "Terminator", release_date: "1999" },
+    { id: 3, title: "Shrek", release_date: "2015" },
+    { id: 4, title: "Justice League", release_date: "2019" },
+    { id: 5, title: "Cinderelle", release_date: "2018" },
+  ];
+  const handleSearch = () => {};
+  return (
+    <div className="home">
+      <form action="" onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+        />
+        <button type="submit" className="search-button">Search</button>
+      </form>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
